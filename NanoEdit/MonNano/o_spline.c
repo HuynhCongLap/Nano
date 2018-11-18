2ac1184d6d7d236ebf6aa30467d5307538bb9d99
HuynhCongLap <huynh.lap.co@gmail.com>
2018-11-18 14:26:01 +0100

    Spline

--- NanoEdit/MonNano/o_spline.c
+++ NanoEdit/MonNano/o_spline.c
@@ -5,14 +5,15 @@
 #include "t_geometrie.h"
 #include "figure.h"
 
-struct bezier
+struct spline
 {
 
-  Table_triplet bezier1;
-
-  Table_triplet points;
+  Table_triplet curve;
+  Table_triplet control_points;
+  Table_flottant weights;
 
   int nb_points;
+  int order;
 } ;
 
 
@@ -26,51 +27,52 @@ float  Coff(int n, int i)
 	return factorial(n)/ (factorial(n-i)* factorial(i));
 }
 
-static void changement(struct bezier *o)
-{ 
+static void changement(struct spline *o)
+{
 
 
   if ( ! (UN_CHAMP_CHANGE(o)||CREATION(o)) )
     return ;
-  
-  if (CHAMP_CHANGE(o,nb_points) || CHAMP_CHANGE(o, points))
+
+  if (CHAMP_CHANGE(o,nb_points) || CHAMP_CHANGE(o, control_points))
   {
-    if (o->bezier1.nb > 0 )
-      free(o->bezier1.table);
+    if (o->curve.nb > 0 )
+      free(o->curve.table);
+
     if (o->nb_points < 2)
       o->nb_points = 10;
 
 
     /*
      *      o->rond.table = malloc(o->nb_points*sizeof(Triplet));
-     *      ALLOUER(o->rond.table,o->nb_points);  
+     *      ALLOUER(o->rond.table,o->nb_points);
      *      for (i=0 ; i<o->nb_points ; i++)
      *	     {
      *        o->rond.table[i].x = points_sur_cercle[3*i];
              }
-     free(points_sur_cercle);   
+     free(points_sur_cercle);
      */
 
-    o->bezier1.table = malloc(o->nb_points*sizeof(Triplet));
-    ALLOUER(o->bezier1.table,o->nb_points);
+    o->curve.table = malloc(o->nb_points*sizeof(Triplet));
+    ALLOUER(o->curve.table,o->nb_points);
 
-    float t = 0; 
+    float t = 0;
     for(int i=0 ; i<o->nb_points ; i++)
     {
-	o->bezier1.table[i].x = 0; // ?????
-	o->bezier1.table[i].y = 0;
-	o->bezier1.table[i].z = 0;
-	int n = o->points.nb -1; 
-	for(int ii=0; ii<o->points.nb; ii++)
+	o->curve.table[i].x = 0; // ?????
+	o->curve.table[i].y = 0;
+	o->curve.table[i].z = 0;
+	int n = o->control_points.nb -1;
+	for(int ii=0; ii<o->control_points.nb; ii++)
 	{
-	  o->bezier1.table[i].x += Coff(n,ii)*pow((1-t),n-ii)*pow(t,ii)*o->points.table[ii].x ;
-	  o->bezier1.table[i].y += Coff(n,ii)*pow((1-t),n-ii)*pow(t,ii)*o->points.table[ii].y ;
-	  o->bezier1.table[i].z += Coff(n,ii)*pow((1-t),n-ii)*pow(t,ii)*o->points.table[ii].z ;	
+	  o->curve.table[i].x += Coff(n,ii)*pow((1-t),n-ii)*pow(t,ii)*o->control_points.table[ii].x ;
+	  o->curve.table[i].y += Coff(n,ii)*pow((1-t),n-ii)*pow(t,ii)*o->control_points.table[ii].y ;
+	  o->curve.table[i].z += Coff(n,ii)*pow((1-t),n-ii)*pow(t,ii)*o->control_points.table[ii].z ;
 	}
 
        t+=(1.0/(o->nb_points-1));
     }
-    
+
     printf("dans changement\n");
   }
   else
@@ -78,32 +80,29 @@ static void changement(struct bezier *o)
 }
 
 
-static void affiche_bezier(struct bezier *o)
+static void affiche_spline(struct spline *o)
 {
-  
+
   glBegin(GL_LINE_STRIP) ;
   for(int j=0  ; j<o->nb_points ; j++)
-    glVertex3f(o->bezier1.table[j].x,o->bezier1.table[j].y,o->bezier1.table[j].z); 
+    glVertex3f(o->curve.table[j].x,o->curve.table[j].y,o->curve.table[j].z);
   glEnd();
 }
 
 
 
 
-CLASSE(bezier, struct bezier,      
-       
-       CHAMP(bezier1, L_table_point P_table_triplet Sauve)
-       CHAMP(nb_points, LABEL("Nombre de points") 
-       L_entier  Edite Sauve DEFAUT("100") )
-       
-       CHAMP(points,L_table_point P_table_triplet Extrait Obligatoire Edite)
+CLASSE(spline, struct spline,
 
-      
-       
+       CHAMP(curve, L_table_point P_table_triplet Sauve)
+       CHAMP(nb_points, LABEL("Nombre de points") L_entier  Edite Sauve DEFAUT("100") )
+       CHAMP(order, LABEL("Order") L_entier  Edite Sauve DEFAUT("3") )
+       CHAMP(control_points, LABEL("Control Points") L_table_point P_table_triplet Extrait Obligatoire Edite)
+       CHAMP(weights, LABEL("Weights") L_table_nombre P_table_flottant Edite Affiche)
+
        CHANGEMENT(changement)
-       CHAMP_VIRTUEL(L_affiche_gl(affiche_bezier))
-       
+       CHAMP_VIRTUEL(L_affiche_gl(affiche_spline))
+
        MENU("Exemples_MG/spline")
        EVENEMENT("Ctrl+BP")
        )
-       
